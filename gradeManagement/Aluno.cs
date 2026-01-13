3b6c856c1011dedaa50f6ee743ee0fb74ed0c6ac
Marney Melo <marney.melo@outlook.com>
2026-01-13 18:46:18 -0300

    Adicionar arquivos de projeto.

---
+++ gradeManagement/Aluno.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Aluno
+{
+    public string Nome { get; set; } = string.Empty;
+    public double[] Notas { get; set; } = new double[5];
+    public double Frequencia { get; set; }
+    
+    //robustez para previnir erros de calculo causados pela entrada.
+    public double MediaNotas => Notas.Length > 0 ? Notas.Average() : 0;
+}
